42833330c8fa826ebbcd3ec080db413740237332
马懿元 <27765097+sparrowMa@users.noreply.github.com>
2017-12-26 22:07:40 -0800

    Initial Commit

---
+++ tipper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipper
+//
+//  Created by 马懿元 on 12/26/17.
+//  Copyright © 2017 Sparrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
